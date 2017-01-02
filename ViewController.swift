b12159e9ab829a08d012a9eabddf608b483b43ba
Travis Hafner <travisdhafner@gmail.com>
2017-01-02 11:38:04 -0500

    boilerplate code

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecretMessage
+//
+//  Created by Travis Hafner on 1/2/17.
+//  Copyright © 2017 Travis Hafner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
